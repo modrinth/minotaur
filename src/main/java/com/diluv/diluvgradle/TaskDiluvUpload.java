0ca0b9fd5e9c1a8b2258852e84537e3f80a6f365
Tyler Hancock <darklime@live.ca>
2020-08-14 04:42:59 -0600

    Implemented the uploading functionality.

--- src/main/java/com/diluv/diluvgradle/TaskDiluvUpload.java
+++ src/main/java/com/diluv/diluvgradle/TaskDiluvUpload.java
@@ -1,9 +1,19 @@
 package com.diluv.diluvgradle;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import javax.annotation.Nullable;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.Project;
 import org.gradle.api.tasks.TaskAction;
@@ -13,43 +23,116 @@ import org.gradle.api.tasks.bundling.AbstractArchiveTask;
  * A task used to communicate with Diluv for the purpose of uploading build artifacts.
  */
 public class TaskDiluvUpload extends DefaultTask {
-    
+            
     /**
      * The URL used for communicating with Diluv. This should not be changed unless you know
      * what you're doing. It's main use case is for debug, development, or advanced user
      * configurations.
      */
-    private String apiURL = "https://api.diluv.com/";
+    public String apiURL = "https://api.diluv.com/v1/projects/files";
     
     /**
-     * The API token used to communicate with Diluv. Make sure you keep this private!
+     * The API token used to communicate with Diluv. Make sure you keep this public!
      */
-    private String token;
+    public String token;
     
     /**
      * The ID of the project to upload the file to.
      */
-    private int projectId;
+    public int projectId;
+    
+    /**
+     * The version of the project being uploaded.
+     */
+    public String projectVersion;
+    
+    /**
+     * The change log data to associate with the new file.
+     */
+    public String changelog;
     
     /**
      * The upload artifact file. This can be any object type that is resolvable by
      * {@link #resolveFile(Project, Object, File)}.
      */
-    private Object uploadFile;
+    public Object uploadFile;
+
+    /**
+     * The release type for the project.
+     */
+    // TODO get a list of valid types
+    public String releaseType = "alpha";
     
     /**
-     * The change log data to associate with the new file.
+     * The type of file being uploaded.
      */
-    private String changelog;
+    public String classifier = "binary";
+    
+    /**
+     * The version of the game the file supports.
+     */
+    // TODO validate this.
+    public String gameVersion;
+    
+    // TODO how to format this
+    public String dependencies;
     
     @TaskAction
     public void apply () {
         
-        System.out.println("API: " + this.apiURL);
-        System.out.println("Token: " + this.token);
-        System.out.println("Project ID: " + this.projectId);
-        System.out.println("File file: " + resolveFile(this.getProject(), this.uploadFile, null));
-        System.out.println("Changelog: " + this.changelog);
+        try {
+            
+            this.upload();
+        }
+        
+        catch (URISyntaxException | IOException e) {
+            
+            // TODO handle errors better. Build failure?
+            this.getProject().getLogger().error("Failure to upload file!", e);
+        }
+    }
+    
+    public void upload() throws URISyntaxException, IOException {
+        
+        final File file = resolveFile(this.getProject(), this.uploadFile, null);
+        this.getProject().getLogger().debug("Uploading {} to {}.", file.getPath(), this.apiURL);
+        
+        final HttpClient client = HttpClientBuilder.create().setDefaultRequestConfig(RequestConfig.custom().setCookieSpec(CookieSpecs.IGNORE_COOKIES).build()).build();
+        final HttpPost post = new HttpPost(new URI(this.apiURL));
+
+        post.addHeader("Authorization", "Bearer " + this.token);
+        
+        final MultipartEntityBuilder form = MultipartEntityBuilder.create();
+        form.addTextBody("project_id", Long.toString(this.projectId));
+        form.addTextBody("version", this.projectVersion);
+        form.addTextBody("changelog", this.changelog);
+        form.addBinaryBody("file", file);
+        form.addTextBody("filename", file.getName());
+        form.addTextBody("releaseType", this.releaseType);
+        form.addTextBody("classifier", this.classifier);
+        form.addTextBody("game_versions", this.gameVersion);
+        
+        if (this.dependencies != null) {
+            
+            form.addTextBody("dependencies", this.dependencies);
+        }
+        
+        post.setEntity(form.build());
+        
+        try {
+            
+            // TODO Expose response data to the script.
+            // TODO Check response and post success/fail lifecycle message.
+            final HttpResponse response = client.execute(post);
+                        
+            this.getProject().getLogger().info("Sucessfully uploaded {} to {}.", file.getName(), this.projectId);
+        }
+        
+        catch (IOException e) {
+
+            this.getProject().getLogger().error("Failure to upload file!", e);
+            throw e;
+        }
     }
     
     /**
@@ -87,5 +170,11 @@ public class TaskDiluvUpload extends DefaultTask {
         
         // Fallback to Gradle's built in file resolution mechanics.
         return project.file(in);
+    }
+
+    @Override
+    public String toString () {
+        
+        return "TaskDiluvUpload [apiURL=" + apiURL + ", token=" + (token != null) + ", projectId=" + projectId + ", projectVersion=" + projectVersion + ", changelog=" + changelog + ", uploadFile=" + uploadFile + ", releaseType=" + releaseType + ", classifier=" + classifier + ", gameVersion=" + gameVersion + ", dependencies=" + dependencies + "]";
     }
 }
