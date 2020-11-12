768f477c394e294143e9eeafbbd24f9fb786dead
Tyler Hancock <darklime@live.ca>
2020-11-12 05:11:05 -0700

    File formatting fixes.

--- src/main/java/com/diluv/diluvgradle/request/RequestData.java
+++ src/main/java/com/diluv/diluvgradle/request/RequestData.java
@@ -1,82 +1,94 @@
 package com.diluv.diluvgradle.request;
 
-import com.google.gson.annotations.Expose;
-import com.google.gson.annotations.SerializedName;
-
 import java.util.ArrayList;
 import java.util.List;
 
-public class RequestData {
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 
+public class RequestData {
+    
     @Expose
     @SerializedName("version")
     public String version;
-
+    
     @Expose
     @SerializedName("changelog")
     public String changelog;
-
+    
     @Expose
     @SerializedName("releaseType")
     public String releaseType;
-
+    
     @Expose
     @SerializedName("classifier")
     public String classifier;
-
+    
     @Expose
     @SerializedName("gameVersions")
     public List<String> gameVersions = new ArrayList<>();
-
+    
     @Expose
     @SerializedName("dependencies")
     public List<FileDependency> dependencies = new ArrayList<>();
-
-    public String getVersion() {
+    
+    public String getVersion () {
+        
         return this.version;
     }
-
-    public void setVersion(String version) {
+    
+    public void setVersion (String version) {
+        
         this.version = version;
     }
-
-    public String getChangelog() {
+    
+    public String getChangelog () {
+        
         return this.changelog;
     }
-
-    public void setChangelog(String changelog) {
+    
+    public void setChangelog (String changelog) {
+        
         this.changelog = changelog;
     }
-
-    public String getReleaseType() {
+    
+    public String getReleaseType () {
+        
         return this.releaseType;
     }
-
-    public void setReleaseType(String releaseType) {
+    
+    public void setReleaseType (String releaseType) {
+        
         this.releaseType = releaseType;
     }
-
-    public String getClassifier() {
+    
+    public String getClassifier () {
+        
         return this.classifier;
     }
-
-    public void setClassifier(String classifier) {
+    
+    public void setClassifier (String classifier) {
+        
         this.classifier = classifier;
     }
-
-    public List<String> getGameVersions() {
+    
+    public List<String> getGameVersions () {
+        
         return this.gameVersions;
     }
-
-    public void setGameVersions(List<String> gameVersions) {
+    
+    public void setGameVersions (List<String> gameVersions) {
+        
         this.gameVersions = gameVersions;
     }
-
-    public List<FileDependency> getDependencies() {
+    
+    public List<FileDependency> getDependencies () {
+        
         return this.dependencies;
     }
-
-    public void setDependencies(List<FileDependency> dependencies) {
+    
+    public void setDependencies (List<FileDependency> dependencies) {
+        
         this.dependencies = dependencies;
     }
 }
