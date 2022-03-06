e327eaa81e231a0d6d4b56248ca7c83d82b0f779
Emma Waffle <emmaffle@modrinth.com>
2022-03-05 22:56:12 -0500

    Fix uploadInfo and Loom game version detection
    - Fix game version detection with Loom
    - Fix uploadInfo and related methods in TaskModrinthUpload by removing the afterEvaluate block

--- src/main/java/com/modrinth/minotaur/Minotaur.java
+++ src/main/java/com/modrinth/minotaur/Minotaur.java
@@ -7,43 +7,23 @@ import org.gradle.api.tasks.TaskContainer;
 /**
  * The main class for Minotaur.
  */
-@SuppressWarnings("unused")
 public class Minotaur implements Plugin<Project> {
     /**
-     * Does the following:
-     * <ol>
-     *   <li>Creates the {@link ModrinthExtension} for the project</li>
-     *   <li>Waits until the project is done evaluating</li>
-     *   <li>Registers the {@code modrinth} task</li>
-     *   <li>Detects the game versions for Fabric and Forge</li>
-     *   <li>Sets default values that couldn't be done without an evaluated project (version number and name)</li>
-     *   <li>Prints a debug line saying it's done!</li>
-     * </ol>
+     * Creates the {@link ModrinthExtension} for the project and registers the {@code modrinth} task
      * @param project The Gradle project which Minotaur is applied to
      */
     @Override
     public void apply(final Project project) {
         project.getExtensions().create("modrinth", ModrinthExtension.class, project);
+        project.getLogger().debug("Created the `modrinth` extension.");
 
-        project.afterEvaluate(evaluatedProject -> {
-            TaskContainer tasks = evaluatedProject.getTasks();
-            tasks.register("modrinth", TaskModrinthUpload.class, task -> {
-                task.setGroup("publishing");
-                task.setDescription("Upload project to Modrinth");
-                task.dependsOn(tasks.named("build"));
-            });
-
-            TaskModrinthUpload.detectGameVersionForge(evaluatedProject);
-            TaskModrinthUpload.detectGameVersionFabric(evaluatedProject);
-
-            final ModrinthExtension extension = evaluatedProject.getExtensions().getByType(ModrinthExtension.class);
-            if (extension.getVersionNumber().getOrNull() == null) {
-                extension.getVersionNumber().set(evaluatedProject.getVersion().toString());
-            }
-            if (extension.getVersionName().getOrNull() == null) {
-                extension.getVersionName().set(extension.getVersionNumber().get());
-            }
+        TaskContainer tasks = project.getTasks();
+        tasks.register("modrinth", TaskModrinthUpload.class, task -> {
+            task.setGroup("publishing");
+            task.setDescription("Upload project to Modrinth");
+            task.dependsOn(tasks.named("build"));
         });
+        project.getLogger().debug("Registered the `modrinth` task.");
 
         project.getLogger().debug("Successfully applied the Modrinth plugin!");
     }
