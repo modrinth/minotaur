c355599ff502bdb2bc0451f512bf7f933c8c0a73
Emma Waffle <emmaffle@modrinth.com>
2022-03-10 16:32:22 -0500

    Add ability to sync file/README with Modrinth project body
    Resolves #3
    
    Adds a new `modrinthSyncBody` task for syncing a string (README file or similar can easily be used with groovy's `.text`) with the Modrinth project body.
    Heavily inspired by @LambdAurora's implementation in various buildscripts of hers (Inspecio is what I used for reference)

--- src/main/java/com/modrinth/minotaur/Minotaur.java
+++ src/main/java/com/modrinth/minotaur/Minotaur.java
@@ -9,7 +9,8 @@ import org.gradle.api.tasks.TaskContainer;
  */
 public class Minotaur implements Plugin<Project> {
     /**
-     * Creates the {@link ModrinthExtension} for the project and registers the {@code modrinth} task
+     * Creates the {@link ModrinthExtension} for the project and registers the {@code modrinth} task.
+     * Also registers the `modrinthSyncBody` task if `syncBodyFrom` is specified in {@link ModrinthExtension}.
      * @param project The Gradle project which Minotaur is applied to
      */
     @Override
@@ -24,6 +25,20 @@ public class Minotaur implements Plugin<Project> {
             task.dependsOn(tasks.named("build"));
         });
         project.getLogger().debug("Registered the `modrinth` task.");
+
+        project.afterEvaluate(evaluatedProject -> {
+            ModrinthExtension extension = evaluatedProject.getExtensions().getByType(ModrinthExtension.class);
+
+            if (extension.getSyncBodyFrom().getOrNull() != null) {
+                tasks.register("modrinthSyncBody", TaskModrinthSyncBody.class, task -> {
+                    task.setGroup("publishing");
+                    task.setDescription("Sync project description to Modrinth");
+                });
+                project.getLogger().debug("Registered the `modrinthSyncBody` task.");
+            } else {
+                project.getLogger().debug("Did not register `modrinthSyncBody` task because there was no `syncBodyFrom` in extension.");
+            }
+        });
 
         project.getLogger().debug("Successfully applied the Modrinth plugin!");
     }
