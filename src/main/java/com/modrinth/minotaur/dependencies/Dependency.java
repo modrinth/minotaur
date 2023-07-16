d88762f708f5cfe1c190d2f54ea5eb43d27f1181
Emma Triphora <emma@modrinth.com>
2023-07-16 12:43:23 -0400

    Fix #55

--- src/main/java/com/modrinth/minotaur/dependencies/Dependency.java
+++ src/main/java/com/modrinth/minotaur/dependencies/Dependency.java
@@ -2,17 +2,14 @@ package com.modrinth.minotaur.dependencies;
 
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
-import com.modrinth.minotaur.ModrinthExtension;
-import masecla.modrinth4j.endpoints.version.GetProjectVersions.GetProjectVersionsRequest;
 import masecla.modrinth4j.main.ModrinthAPI;
 import masecla.modrinth4j.model.version.ProjectVersion;
 import masecla.modrinth4j.model.version.ProjectVersion.ProjectDependency;
 import masecla.modrinth4j.model.version.ProjectVersion.ProjectDependencyType;
 import org.gradle.api.GradleException;
 import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.Nullable;
 
-import java.util.List;
+import java.util.AbstractMap.SimpleEntry;
 import java.util.Locale;
 import java.util.Objects;
 
@@ -57,10 +54,9 @@ public class Dependency {
 
 	/**
 	 * @param api {@link ModrinthAPI} instance
-	 * @param ext {@link ModrinthExtension} instance
 	 * @return a {@link ProjectDependency} instance from a {@link Dependency}
 	 */
-	public ProjectDependency toNew(ModrinthAPI api, ModrinthExtension ext) {
+	public ProjectDependency toNew(ModrinthAPI api) {
 		if (this instanceof ModDependency) {
 			ModDependency dep = (ModDependency) this;
 			String id = Objects.requireNonNull(
@@ -70,8 +66,16 @@ public class Dependency {
 			return new ProjectDependency(null, id, null, dep.getDependencyType());
 		} else if (this instanceof VersionDependency) {
 			VersionDependency dep = (VersionDependency) this;
-			String versionId = resolveVersionId(dep.getProjectId(), dep.getVersionId(), api);
-			return new ProjectDependency(versionId, dep.getProjectId(), null, dep.getDependencyType());
+			SimpleEntry<String, String> projectInfo;
+			try {
+				ProjectVersion version = dep.getProjectId() == null
+					? api.versions().getVersion(dep.getVersionId()).join()
+					: api.versions().getVersionByNumber(dep.getProjectId(), dep.getVersionId()).join();
+				projectInfo = new SimpleEntry<>(version.getProjectId(), version.getId());
+			} catch (Exception e) {
+				throw new GradleException("Failed to resolve version \"" + dep.getVersionId() + "\"!", e);
+			}
+			return new ProjectDependency(projectInfo.getValue(), projectInfo.getKey(), null, dep.getDependencyType());
 		} else {
 			throw new GradleException("Dependency was not an instance of ModDependency or VersionDependency!");
 		}
@@ -86,23 +90,6 @@ public class Dependency {
 			return new VersionDependency(newDep.getProjectId(), newDep.getVersionId(), newDep.getDependencyType().name());
 		} else {
 			return new ModDependency(newDep.getProjectId(), newDep.getDependencyType().name());
-		}
-	}
-
-	/**
-	 * Returns a project ID from a project ID or slug
-	 *
-	 * @param versionId ID or version number of the project to resolve
-	 * @return ID of the resolved project
-	 */
-	private String resolveVersionId(@Nullable String projectId, String versionId, ModrinthAPI api) {
-		try {
-			ProjectVersion version = projectId == null
-				? api.versions().getVersion(versionId).join()
-				: api.versions().getVersionByNumber(projectId, versionId).join();
-			return version.getId();
-		} catch (Exception e) {
-			throw new GradleException("Failed to resolve version \"" + versionId + "\"!", e);
 		}
 	}
 }
